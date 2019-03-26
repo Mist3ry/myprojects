fd7073b750f14d93e1670be1079967ea7c6c8c53
Kappa <33200836+Mist3ry@users.noreply.github.com>
2019-03-26 15:10:58 +0100

    Update game.java

--- OddsOrEven/game.java
+++ OddsOrEven/game.java
@@ -5,8 +5,10 @@ public class game {
         greeding();
     }
 
-    //simple game vs computer, you choose what will win Odd or Even number, put your number, than computer adds random number up to 6
-    //sum the numbers, makes conclusion is it even or odd and if you`ve chosen even and the sum is also even you win, else you lose.
+    //The project represtnt simple game vs computer. First of all you decide what you will play at that game even or not even. 
+    //Computer takes the opposite. Then you guessing numbers and the computer in turn also generates a random number up to 6. 
+    //Numbers are added together and it is checked whether this number is even or not. 
+    //If you guessed right it and the amount came out of the one you asked earlier, you won, else you lose.
     public static void greeding() {
         Scanner action = new Scanner(System.in);
         System.out.print("What is your name? ");
